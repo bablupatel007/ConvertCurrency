847722f30691e24d7ef172a76f68f696176777b0
Bablu <bablu.patel@ibm.com>
2021-08-10 23:30:13 +0530

    Initial commit

---
+++ src/main/java/com/ibm/activity/convertcurrency/client/ConvertCurrencyRestClient.java
@@ -0,0 +1,18 @@
+package com.ibm.activity.convertcurrency.client;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ibm.activity.convertcurrency.dto.ConvertCurrencyDTO;
+
+
+@FeignClient(name="CurrencyConversionFactor") //url="192.168.99.100:8088"
+@RibbonClient(name="CurrencyConversionFactor")
+public interface ConvertCurrencyRestClient {
+
+	@GetMapping("/currencyconversion/country/{countryCode}")
+	public ConvertCurrencyDTO getConversionFactor(
+			@RequestParam(value = "countryCode") String countryCode);
+}
